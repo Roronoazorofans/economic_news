278679b8dede2e55aa3091ed26f485095744420f
zorozhang <zorozhang@163.com>
2018-05-24 20:57:20 +0800

    开启csrf保护和配置session

--- manage.py
+++ manage.py
@@ -2,6 +2,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from redis import StrictRedis
+from flask_wtf.csrf import CSRFProtect
+from flask_session import Session
 
 
 app = Flask(__name__)
@@ -13,11 +15,25 @@ class Config(object):
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     REDIS_HOST = "127.0.0.1"
     REDIS_PORT = 6379
+    # 配置csrf秘钥 使用一组随机数作为'盐'
+    SECRET_KEY = "q7pBNcWPgmF6BqB6b5VICF7z7pI+90o0O4CaJsFGjzRsYiya9SEgUDytXvzFsIaR"
+    # 配置session数据
+    # 配置存储session数据的数据库类型为redis
+    SESSION_TYPE = "redis"
+    # 开启用户签名
+    SESSION_USE_SIGNER = True
+    # 设置session有效期 为一天
+    PERNAMENT_SESSION_LIFETIME = 3600*24
 
 # 加载配置文件
 app.config.from_object(Config)
 # 创建连接到mysql数据库的对象
 db = SQLAlchemy(app)
+# 开启csrf保护
+CSRFProtect(app)
+Session(app)
+
+
 redis_store = StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 @app.route('/')
 def index():
