11fab35e3e3b120728b0927ba32efba96697a987
zorozhang <zorozhang@163.com>
2018-05-25 10:12:59 +0800

    对全局db的处理,请看SQLAlchemy源代码

--- info/__init__.py
+++ info/__init__.py
@@ -3,8 +3,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 from flask_session import Session
-from config import Config, configs
+from config import configs
 
+# 对全局db的处理,请看SQLAlchemy源代码
+# 创建SQLAlchemy对象
+db = SQLAlchemy()
 
 
 # 工厂方法创建app
@@ -12,8 +15,9 @@ def create_app(config_name):
     app = Flask(__name__)
     # 加载配置文件
     app.config.from_object(configs[config_name])
-    # 创建连接到mysql数据库的对象
-    db = SQLAlchemy(app)
+    # # 创建连接到mysql数据库的对象
+    # db = SQLAlchemy(app)
+    db.init_app(app)
     # 开启csrf保护
     CSRFProtect(app)
     Session(app)
--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ from redis import StrictRedis
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 from config import Config
-from info import create_app
+from info import create_app, db
 
 app = create_app('dev')
 # 集成flask_script
