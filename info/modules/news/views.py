96a04edfaebb438caf1e3b2906127206b9da410f
zorozhang <zorozhang@163.com>
2018-06-02 19:08:18 +0800

    新闻评论和评论回复

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,15 +1,75 @@
 # coding=utf-8
 from . import news_blue
 from flask import render_template, session, current_app, g, abort, jsonify, request
-from info.models import User, News
+from info.models import User, News, Comment
 from info import constants, db, response_code
 from info.utils.comment import user_login_data
+
+
+# 新闻评论
+@news_blue.route('/news_comment', methods=['POST'])
+@user_login_data
+def news_comment():
+    """只有在用户登录的情况下才能进行评论操作
+
+    """
+    user = g.user
+    if not user:
+        return jsonify(errno=response_code.RET.SESSIONERR, errmsg='用户未登录')
+    # 接收请求参数 1.被评论新闻id 2.评论内容
+    news_id = request.json.get('news_id')
+    comment_context = request.json.get("comment")
+    parent_id = request.json.get('parent_id')
+    # 校验参数
+    # parent_id 非必要
+    if not all([news_id, comment_context]):
+        return jsonify(errno=response_code.RET.PARAMERR, errmsg='缺少参数')
+    # 将news_id和parent_id(如果其存在)转成整型,目的是为了限制参数格式
+    try:
+        news_id = int(news_id)
+        if parent_id:
+            parent_id = int(parent_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=response_code.RET.PARAMERR, errmsg='缺少参数')
+    # 查询新闻是否存在
+    try:
+        news = News.query.get(news_id)
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=response_code.RET.DBERR, errmsg='查询新闻数据失败')
+    if not news:
+        return jsonify(errno=response_code.RET.NODATA, errmsg='新闻不存在')
+
+    # 创建评论对象
+    comment = Comment()
+    # 给评论对象赋值
+    comment.user_id = user.id
+    comment.news_id = news_id
+    comment.content = comment_context
+    if parent_id:
+        comment.parent_id = parent_id
+    # 将评论数据存储入数据库
+    try:
+        db.session.add(comment)
+        db.session.commit()
+    except Exception as e:
+        current_app.logger.error(e)
+        db.session.rollback()
+        return jsonify(errno=response_code.RET.DBERR, errmsg='存储评论数据失败')
+    return jsonify(errno=response_code.RET.OK, errmsg='ok', data=comment.to_dict())
+
 
 @news_blue.route('/news_collect',methods=['POST'])
 @user_login_data
 def news_collect():
     """只有在用户登录下才能收藏
-
+    1.获取登录用户信息，因为收藏和取消收藏都是需要在登录状态下执行的
+    2.接受参数
+    3.检验参数
+    4.查询当前要收藏或取消收藏的新闻是否存在
+    5.实现收藏和取消收藏
+    6.响应收藏和取消收藏的结果
     """
     # 获取用户信息
     user = g.user
@@ -97,11 +157,24 @@ def news_detail(news_id):
             is_collected = True
 
 
+    # 展示新闻评论数据
+    # 根据新闻id查询该新闻的所有评论数据
+    comments = None
+    try:
+        comments = Comment.query.filter(Comment.news_id == news_id).order_by(Comment.create_time.desc()).all()
+    except Exception as e:
+        current_app.logger.error(e)
+    comment_dict_list = []
+    for comment in comments:
+        comment_dict_list.append(comment.to_dict())
+
+
     context = {
         "user":user,
         "news_clicks":news_clicks,
         "news":news.to_dict(),
-        "is_collected":is_collected
+        "is_collected":is_collected,
+        "comments":comment_dict_list
     }
 
     return render_template('news/detail.html',context=context)
