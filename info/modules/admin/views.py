45de85beae2ce805b4c69036847b4acc62b90adc
zorozhang <zorozhang@163.com>
2018-06-04 22:37:20 +0800

    新闻后台之管理员登录前后端逻辑

--- info/modules/admin/views.py
+++ info/modules/admin/views.py
@@ -1,8 +1,61 @@
 # coding=utf-8
 from info.modules.admin import admin_blue
-from flask import render_template
+from flask import render_template,request, current_app, session, redirect, url_for, g
+from info.models import User
+from info.utils.comment import user_login_data
 
-@admin_blue.route('/login')
+@admin_blue.route('/index')
+@user_login_data
+def admin_index():
+    user = g.user
+    if not user:
+        return redirect(url_for('admin.admin_login'))
+    context = {
+        'user':user.to_dict()
+    }
+    return render_template('admin/index.html', context=context)
+
+@admin_blue.route('/login',methods=['GET','POST'])
 def admin_login():
+    """管理员用户登录"""
+    if request.method == 'GET':
+        user_id = session.get('user_id', None)
+        is_admin = session.get('is_admin', False)
+        if user_id and is_admin:
+            return redirect(url_for('admin.admin_index'))
+        return render_template('admin/login.html')
+    if request.method == 'POST':
+        # 1. 接受参数
+        username = request.form.get('username')
+        password = request.form.get('password')
+        # 2. 校验参数
+        if not all([username, password]):
+            return render_template('admin/login.html', errmsg='参数不足')
 
-    return render_template('admin/login.html')
+        # 3. 校验用户名和密码是否正确
+        try:
+            user = User.query.filter(User.nick_name == username).first()
+        except Exception as e:
+            current_app.logger.error(e)
+            return render_template('admin/login.html', errmsg='查询用户数据失败')
+        if not user:
+            return render_template('admin/login.html', errmsg='用户名或密码错误')
+        if not user.check_passowrd(password):
+            return render_template('admin/login.html', errmsg='用户名或密码错误')
+        if not user.is_admin:
+            return render_template('admin/login.html', errmsg='用户权限错误')
+
+        # 写入状态保持信息到session
+        session['nick_name'] = user.nick_name
+        session['mobile'] = user.mobile
+        session['is_admin'] = user.is_admin
+        session['user_id'] = user.id
+
+        context = {
+            'nick_name': user.nick_name,
+
+        }
+
+
+        # 5. 如果校验通过即进入管理员主界面
+        return render_template('admin/index.html', context=context)
