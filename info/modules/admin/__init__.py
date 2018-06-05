e9216884f26cf221f81ab8de3a6ec5f6625c83df
zorozhang <zorozhang@163.com>
2018-06-05 11:01:09 +0800

    后台每次请求前判断是否是管理员+放开登录权限+ 前台退出时清理is_admin

--- info/modules/admin/__init__.py
+++ info/modules/admin/__init__.py
@@ -1,5 +1,14 @@
 # coding=utf-8
-from flask import Blueprint
+from flask import Blueprint, session, redirect, url_for, request
 admin_blue = Blueprint('admin',__name__,url_prefix='/admin')
 
 from . import views
+
+@admin_blue.before_request
+def check_admin():
+    """在每次请求前判断是否是管理员用户"""
+    is_admin = session.get('is_admin', False)
+    # 注意：当只加第一个条件时会在普通用户登录状态下把进入127.0.0.1/admin/login的入口也给封住,因为在请求之前也进行了判断，此时就需要加入第二个判断条件
+
+    if not is_admin and not request.url.endswith('/admin/login'):
+        return redirect(url_for('index.index'))
